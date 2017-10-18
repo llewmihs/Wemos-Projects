783fbc213c4aea6a6009e0879a118accef6fb161
llewmihs <joeshimwell@gmail.com>
2017-10-18 19:39:45 +0100

    PLaying wth more code

--- Arduino-Sketches/fauxmoglobe/fauxmoglobe.ino
+++ Arduino-Sketches/fauxmoglobe/fauxmoglobe.ino
@@ -1,25 +1,22 @@
 #include <fauxmoESP.h>
 
-/* After you write your sketch and start the ESP, 
-it will try to connect to WiFi. If it fails it starts in Access Point mode. 
-While in AP mode, connect to it then open a browser to the gateway IP, 
-default 192.168.4.1, configure wifi, save and it should reboot and connect.
-*/
-
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
-
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>
 
-
+fauxmoESP fauxmo;
 
 void setup(){
     WiFiManager wifiManager;
     wifiManager.resetSettings();
     wifiManager.autoConnect("AutoConnectAP");
+    fauxmo.addDevice("light one");
+    fauxmo.onMessage([](unsigned char device_id, const char * device_name, bool state) {
+        Serial.printf("[MAIN] Device #%d (%s) state: %s\n", device_id, device_name, state ? "ON" : "OFF");
+    });
 }
 
 void loop(){
-
+  fauxmo.handle();
 }
--- Arduino-Sketches/wemosglobe/wemosglobe.ino
+++ Arduino-Sketches/wemosglobe/wemosglobe.ino
@@ -1,37 +1,41 @@
-// necessaries forthe NeoPixel
-#include <Adafruit_NeoPixel.h>
-#define pixelPin D5
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, pixelPin,NEO_GRB + NEO_KHZ800);
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "fauxmoESP.h"
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 
-void setup(){
-strip.begin();
-strip.show();
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>
 
-Serial.begin(115200);
-
-strip.setPixelColor(0, Wheel(255));
-strip.show();
-
+#define SERIAL_BAUDRATE                 115200
+ 
+fauxmoESP fauxmo;
+ 
+// -----------------------------------------------------------------------------
+// Wifi
+// -----------------------------------------------------------------------------
+ 
+void setup() {
+ 
+    // Init serial port and clean garbage
+    Serial.begin(SERIAL_BAUDRATE);
+    Serial.println();
+    Serial.println();
+ 
+    // Wifi
+    WiFiManager wifiManager;
+    wifiManager.resetSettings();
+    wifiManager.autoConnect("AutoConnectAP");
+ 
+    // Fauxmo v2.0
+    fauxmo.addDevice("light one");
+    fauxmo.addDevice("light two");
+    fauxmo.onMessage([](unsigned char device_id, const char * device_name, bool state) {
+        Serial.printf("[MAIN] Device #%d (%s) state: %s\n", device_id, device_name, state ? "ON" : "OFF");
+    });
+ 
 }
-
-void loop(){
-    for (int i = 0; 1< 255; i++){
-        strip.setPixelColor(0, Wheel(i));
-        strip.show();
-        delay(50);
-    }
+ 
+void loop() {
+    fauxmo.handle();
 }
-
-uint32_t Wheel(byte WheelPos) {
-    WheelPos = 255 - WheelPos;
-    if(WheelPos < 85) {
-     return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-    } else if(WheelPos < 170) {
-      WheelPos -= 85;
-     return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-    } else {
-     WheelPos -= 170;
-     return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-    }
-  }
-  
